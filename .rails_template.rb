7e745a56200b127e3418e9614a0e25f0fa3098ff
Jay Hayes <ur@iamvery.com>
2015-02-17 12:42:29 -0600

    Use --skip-turbolinks option (noice!)

--- .rails_template.rb
+++ .rails_template.rb
@@ -33,11 +33,7 @@ run "bundle binstubs rspec-core"
 commit "Add and configure RSpec with Turnip"
 
 run "sed -i '' '/^.*#/ d' Gemfile"
-# Remove turbolinks
-run "sed -i '' '/turbolinks/ d' Gemfile"
-run "sed -i '' '/turbolinks/ d' app/assets/javascripts/application.js"
-run %q{sed -i '' "s/, 'data-turbolinks-track' => true//" app/views/layouts/application.html.erb}
-commit "Remove turbolinks"
+commit "Remove Gemfile comments"
 
 # TODO Gemfile cleanup
 # TODO Ruby version in gemfile?
