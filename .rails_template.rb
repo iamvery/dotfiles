c9e10af36a17d4b21b3dcb4dedfcd08acc7af280
Jay Hayes <ur@iamvery.com>
2015-02-16 22:36:25 -0600

    Group git commits with commands

--- .rails_template.rb
+++ .rails_template.rb
@@ -10,7 +10,6 @@ gem_group :development, :test do
   gem "dotenv"
   gem "pry-rails"
 end
-
 commit "Add dotenv and pry-rails"
 
 gem_group :test do
@@ -27,7 +26,6 @@ run "wget --directory-prefix=spec https://gist.github.com/iamvery/6c87c9e191d326
 run "wget --directory-prefix=spec https://gist.github.com/iamvery/6c87c9e191d32603aa78/raw/turnip_helper.rb"
 
 run "bundle binstubs rspec-core"
-
 commit "Add and configure RSpec with Turnip"
 
 # Remove comments from Gemfile
@@ -36,7 +34,6 @@ run "sed -i '' '/^.*#/ d' Gemfile"
 run "sed -i '' '/turbolinks/ d' Gemfile"
 run "sed -i '' '/turbolinks/ d' app/assets/javascripts/application.js"
 run %q{sed -i '' "s/, 'data-turbolinks-track' => true//" app/views/layouts/application.html.erb}
-
 commit "Remove turbolinks"
 
 # TODO Gemfile cleanup
@@ -44,16 +41,13 @@ commit "Remove turbolinks"
 # TODO database_cleaner?
 
 run "bundle install"
-
 commit "Bundle gems"
 
 run "wget --directory-prefix=bin https://gist.github.com/iamvery/6c87c9e191d32603aa78/raw/bootstrap"
 run "chmod +x bin/bootstrap"
-
 commit "Add bootstrap script"
 
 run "rm README.rdoc"
 run "wget https://gist.github.com/iamvery/6c87c9e191d32603aa78/raw/README.md"
 run %Q{sed -i '' "s/\\[APP NAME\\]/#{app_path.titleize}/" README.md}
-
 commit "Use markdown readme"
