aca0ad113e039010d900e418e6818c3d57db4a10
Jay Hayes <ur@iamvery.com>
2015-02-16 21:44:52 -0600

    Rename rails spec helper in template

--- .rails_template.rb
+++ .rails_template.rb
@@ -18,7 +18,7 @@ end
 
 run "wget https://gist.github.com/iamvery/6c87c9e191d32603aa78/raw/5052887a0fb55b51cace616cd18577ad181614d4/.rspec"
 run "mkdir spec"
-run "wget --directory-prefix=spec https://gist.github.com/iamvery/6c87c9e191d32603aa78/raw/991cf986e0c2d8c4c878b715ad7cba4674b98bc1/rails_helper.rb"
+run "wget --directory-prefix=spec https://gist.github.com/iamvery/6c87c9e191d32603aa78/raw/991cf986e0c2d8c4c878b715ad7cba4674b98bc1/rails_spec_helper.rb"
 run "wget --directory-prefix=spec https://gist.github.com/iamvery/6c87c9e191d32603aa78/raw/7859ff7aad5c412e2755507d3d8fc442c67b7d4f/spec_helper.rb"
 
 run "bundle binstubs rspec-core"
