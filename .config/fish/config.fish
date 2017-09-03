d3dddc74d73b623fd55befda0ef3813ebd0e3c79
Jay Hayes <ur@iamvery.com>
2017-09-03 10:19:18 -0500

    Add fish-compatible aliases: prune & wip

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,1 +1,5 @@
+# http://stevenharman.net/git-clean-delete-already-merged-branches
+alias prune='git fetch --prune ; and git branch --merged | grep -v "*" | xargs -n 1 git branch -d'
+alias wip='git add . ; and git commit -m "wip [ci skip]"'
+
 source ~/.aliases
