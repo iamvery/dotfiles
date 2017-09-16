2fe9e580d8e2e5ba890421518241581624a41645
Jay Hayes <ur@iamvery.com>
2017-09-16 09:10:07 -0500

    Port over a couple more commonly used aliases to fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,8 @@
+# http://solidfoundationwebdev.com/blog/posts/create-an-bash-function-to-open-latest-active-record-migration
+alias lm='vim db/migrate/(ls db/migrate | sort | tail -1)'
 # http://stevenharman.net/git-clean-delete-already-merged-branches
 alias prune='git fetch --prune ; and git branch --merged | grep -v "*" | xargs -n 1 git branch -d'
+alias gutd='git checkout master ; and git pull --ff-only ; and prune ; and git checkout - ; and git rebase master'
 alias wip='git add . ; and git commit -m "wip [ci skip]"'
 
 source ~/.aliases
