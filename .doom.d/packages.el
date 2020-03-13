3a464c375431149eac601bcace63ee6d5ee985e1
Jay Hayes <ur@iamvery.com>
2020-03-13 05:51:51 -0500

    Install doom-emacs
    https://github.com/hlissner/doom-emacs

---
+++ .doom.d/packages.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; -*-
+;;; .doom.d/packages.el
+
+;;; Examples:
+;; (package! some-package)
+;; (package! another-package :recipe (:host github :repo "username/repo"))
+;; (package! builtin-package :disable t)
+(package! plantuml-mode)
